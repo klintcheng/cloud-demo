8a2378dd3443e3a126655ea4bede6db7bf789d2a
klintcheng <klint1988@icloud.com>
2025-02-17 17:55:46 +0800

    init consul config

--- gateway/src/main/java/com/jmyy/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/jmyy/gateway/GatewayApplication.java
@@ -1,11 +1,15 @@
 package com.jmyy.gateway;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import lombok.extern.log4j.Log4j2;
+
 @SpringBootApplication
 @EnableDiscoveryClient
+@Log4j2
 public class GatewayApplication {
 
 	public static void main(String[] args) {
---
+++ gateway/src/main/java/com/jmyy/gateway/startup/StartupMessagePrinter.java
@@ -0,0 +1,18 @@
+package com.jmyy.gateway.startup;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StartupMessagePrinter implements ApplicationRunner {
+
+    @Value("${custom.message}")
+    private String message;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("启动配置消息: " + message);
+    }
+}
